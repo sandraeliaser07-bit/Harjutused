117031741301bc503364ffa8c5f75013979e7406
Opilane TTHK <opilane@tthk.ee>
2026-01-14 09:03:47 +0200

    puhtandid

---
+++ Meetodid_castimine/CASTING/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace CASTING
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. "pikkus"
+            //kirjuta programm mis
+            //küsib kasutajalt kui pikk ta sentimeetrites on
+            //muundab käsurea pealt sissesaadud arvu täisarvuks
+            //programm tagastab sõnumiga tema pikkuse meetrites, kus komakohti on 2
+
+            //2. "mitu meetrit"
+            //kirjuta programm mis
+            //küsib kasutajalt kui kaugel ta koolist elab kilomeetrites
+            //käsurea pealt saadakse komakohaga arv
+            //programm teisendab selle arvu meetritesse, mis on täisarv
+            //kuvab kasutajale sõnumi, "sa elad <meetreid> meetrit koolist eemal"
+
+            //3. "mis ASCII number on soovitav täht"
+            //kirjuta programm mis
+            //küsib kasutajalt ühtainust tähte
+            //programm kontrollib kas kasutaja sisestus on pikem kui 1 või lühem kui 1
+            //  kui jah, siis kuvab sõnumi, "palun sisesta üksainus täht järgmine kord"
+            //  kui ei, siis teisenda ühetäheline string char-tüüpi andmeks ja
+            //  kuva sõnum, "sinu tähekood on <char>" 
+
+            //4. "otsi tähte numbri järgi
+            //kirjuta programm mis
+            //küsib kasutajalt täisarvu
+            //programm teisendab käsurea pealt saadud arvu täisarvuks
+            //tekita uus char-tüüpi muutuja kuhu omistad sisse char konverteerimisega täisarvu 
+            //kuva sõnum, "sinu otsitud täht on <char>" 
+
+        }
+    }
+}
