edbebac5783e577d04de37e9d9d379948a148cd8
Opilane TTHK <opilane@tthk.ee>
2026-01-29 11:41:29 +0200

    harjutus

---
+++ Meetod/Meetod3/meetod3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace meetod3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Testmethod();
+            int sisend1 = int.Parse(Console.ReadLine());
+            int sisend2 = int.Parse(Console.ReadLine());
+            float tulemus = Arvutaja(sisend1, sisend2);
+            Console.WriteLine(tulemus);
+            // kirjuta meetod mis tagastab kasutajale true või false, kui kasutaja poolt sisestatud arv ületab 100
+
+      
+        }
+
+        public static void TrueFalse()
+        {
+            Console.WriteLine(true);
+            if ()
+
+            
+        }
+        public static void Testmethod()
+        {
+            Console.WriteLine("tere");
+        }
+        public static float Arvutaja(int arv1, int arv2)
+        {
+            return (float)arv1 / (float)arv2;
+        } 
+    }
+}
---
+++ Meetod/Meetoditetöö/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Meetoditetöö
+{
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            
+        }
+        public static void Tervitus()
+        {
+            Console.WriteLine("Tere :)");
+        }
+        int[] arvutatavadarvud = new int[] { 3, 67, 420 };
+
+
+
+    }
+}
